5ff90feb69c94611ee306a6a456a6dd758be3c17
VLaxmikanth <laxmikanth.vattamvar@tibilsolutions.com>
2020-03-10 17:35:09 +0530

    s3 api code change

--- development/angular/server-side/S3_API.js
+++ development/angular/server-side/S3_API.js
@@ -1,14 +1,117 @@
+var express = require('express');
+var app = express();
 const aws = require('aws-sdk');
-require('dotenv').config();
+var cors = require('cors')
+var const_data = require('./aws-config')
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+logger.level = 'debug';
+var temp_key = const_data[1];
+// console.log(const_data);
+// console.log(temp_key);
+app.use(cors())
+app.get('/s3-month-wise', function (req, res) {
 
-const accessKeyId = process.env.ACCESS_KEY_ID;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+    logger.debug("Some debug messages");
+    temp_key['Key']='Output/month-wise.json'
+    const_data[0].getObject(temp_key, function (err, data) {
 
-var s3 = new aws.S3({ 'accessKeyId': accessKeyId, 'secretAccessKey': secretAccessKey });
+        if (err) {
+            console.log(err);
+        } else {
+            const JSONdata = JSON.parse(data.Body.toString())
+            var myDataPoint = [];
+            var keys = Object.keys(JSONdata);
+            for (let i = 0; i < keys.length; i++) {
+                var data = {
+                    value: (JSONdata[i].x_value),
+                    label: (JSONdata[i].x_axis)
+                };
+                myDataPoint.push(data);
+            }
+           
+            res.send(myDataPoint);
+            logger.debug("Some debug messages");
 
-var getParams = {
-    Bucket: 'nifi-test-tibil', //replace example bucket with your s3 bucket name
-    Key: '' // replace file location with your s3 file location
-};
+        }
 
-module.exports = { "s3": s3, "getParams": getParams };
+    })
+})
+
+app.get('/S3-all-school-wise', function (req, res) {
+
+    logger.debug("Some debug messages");
+    temp_key['Key']='Output/all-school-wise.json'
+    const_data[0].getObject(temp_key, function (err, data) {
+        
+        if (err) {
+            console.log(err);
+        } else {
+            const JSONdata = JSON.parse(data.Body.toString())
+            var myDataPoint = [];
+            var keys = Object.keys(JSONdata);
+            for (let i = 0; i < keys.length; i++) {
+                var data = {
+                    value: (JSONdata[i].x_value),
+                    label: (JSONdata[i].x_axis)
+                };
+                myDataPoint.push(data);
+            }
+            res.send(myDataPoint);
+            logger.debug("Some debug messages");
+
+        }
+
+    })
+})
+
+app.get('/S3-kpi', function (req, res) {
+    temp_key['Key']='Output/KPI.json'
+    logger.debug("Some debug messages");
+    const_data[0].getObject(temp_key, function (err, data) {
+
+        if (err) {
+            console.log(err);
+        } else {
+            const JSONdata = JSON.parse(data.Body.toString())
+            
+            res.send(JSONdata);
+
+            logger.debug("Some debug messages");
+
+        }
+
+    })
+});
+
+app.get('/S3-gender-wise', function (req, res) {
+
+    logger.debug("Some debug messages");
+    temp_key['Key']='Output/gender-wise.json'
+    const_data[0].getObject(temp_key, function (err, data) {
+        
+        if (err) {
+            console.log(err);
+        } else {
+            const JSONdata = JSON.parse(data.Body.toString())
+            var myDataPoint = [];
+            var keys = Object.keys(JSONdata);
+            for (let i = 0; i < keys.length; i++) {
+                var data = {
+                    value: (JSONdata[i].x_value),
+                    label: (JSONdata[i].x_axis)
+                };
+                myDataPoint.push(data);
+            }
+            res.send(myDataPoint);
+            logger.debug("Some debug messages");
+
+        }
+
+    })
+})
+
+app.listen(8091, function () {
+
+    console.log("server is running on the port 8091")
+})
