a003527cc17fab322167a2a4044b111a7f0f258b
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2020-04-16 14:02:00 +0530

    Roal based loging implemented

--- development/leaflet/server-side/src/api/controller/block_wise_data.js
+++ development/leaflet/server-side/src/api/controller/block_wise_data.js
@@ -3,7 +3,7 @@ var const_data = require('../config/aws-config');
 const auth = require('../middleware/check-auth');
 
 router.get('/', auth.authController, async (req, res) => {
-    const_data['getParams']['Key'] = 'block-wise-percentage.json'
+    const_data['getParams']['Key'] = '3m_data/block-wise-percentage.json'
     const_data['s3'].getObject(const_data['getParams'], async function (err, data) {
         if (err) {
             console.log(err);
