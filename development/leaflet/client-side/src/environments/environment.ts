499f3e8a3f0eb5c2068613b10cbeaab9c9a4f5c2
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2020-04-17 13:19:16 +0530

    Clickable title hierarchy, bubble size reduced

--- development/leaflet/client-side/src/environments/environment.ts
+++ development/leaflet/client-side/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiEndpoint: "http://localhost:3000/api"
+  apiEndpoint: "https://cqube.tibilprojects.com/api"
 };
 
 // https://cqube.tibilprojects.com
