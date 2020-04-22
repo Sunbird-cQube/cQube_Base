8e7a929f0dcc73ba9e52b692726084855335c9c1
VLaxmikanth <laxmikanth.vattamvar@tibilsolutions.com>
2020-04-22 13:11:46 +0530

    stable angular code

--- development/angular/client-side/src/app/app.module.ts
+++ development/angular/client-side/src/app/app.module.ts
@@ -5,29 +5,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentAttedenceComponent } from './student-attedence/student-attedence.component';
 import { AgmDirectionModule } from 'agm-direction';
 import { LoginComponent } from './login/login.component';
 import { UserViewComponent } from './user-view/user-view.component';
-import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
+import { MapViewComponent } from './map-view/map-view.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentAttedenceComponent,
     LoginComponent,
     UserViewComponent,
+    MapViewComponent,
+    BarChartComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    LeafletModule.forRoot(),
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCbRPhVlxgVwBC0bBOgyB-Dn_K8ONrxb_g' + '&libraries=visualization'
     }),
-    AgmDirectionModule,
-    AgmJsMarkerClustererModule
+    AgmDirectionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
