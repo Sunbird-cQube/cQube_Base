07421dab5aad1947f3bb2fe575fed5c93daebaa4
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2020-05-05 15:07:39 +0530

    Table data at each level done, download functionality is done, color gredient for chart points

--- development/angular/client-side/src/environments/environment.ts
+++ development/angular/client-side/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiEndpoint: "http://localhost:3000/api"
+  apiEndpoint: "http://cqube.tibilprojects.com/api"
 };
 
 // https://cqube.tibilprojects.com
