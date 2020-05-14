2505501cae90ccbd32199ca64e2dccb0df032601
VLaxmikanth <laxmikanth.vattamvar@tibilsolutions.com>
2020-05-14 12:38:57 +0530

    validations added in ansible

--- ansible/installation_scripts/validation_scripts/install_aws_cli.sh
+++ ansible/installation_scripts/validation_scripts/install_aws_cli.sh
@@ -3,11 +3,15 @@
 VAL_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$VAL_DIR" ]]; then VAL_DIR="$PWD"; fi
 
+echo "Installing aws cli ..."
 wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip  > /dev/null 2>&1
-unzip awscli-bundle.zip  
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws 
-./awscli-bundle/install -b ~/bin/aws 
-./awscli-bundle/install -h 
+echo "A" | unzip awscli-bundle.zip  > /dev/null 2>&1 
+echo "Configuring aws cli ..."
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws  > /dev/null 2>&1
+./awscli-bundle/install -b ~/bin/aws  > /dev/null 2>&1
+./awscli-bundle/install -h  > /dev/null 2>&1
+
+echo "Validating aws cli ..."
 
 AWS_DIR=$HOME/.aws
 SYS_USERNAME=`grep 'system_user_name:' $VAL_DIR/../vars/main.yml | awk '{print $2}'`
@@ -15,16 +19,20 @@ ACCESS_KEY=`grep 's3_access_key:' $VAL_DIR/../vars/main.yml | awk '{print $2}'`
 SECRET_KEY=`grep 's3_secret_key:' $VAL_DIR/../vars/main.yml | awk '{print $2}'`
 DEFAULT_REGION=`grep 'aws_default_region:' $VAL_DIR/../vars/main.yml | awk '{print $2}'`
 
-if [ ! -d $AWS_DIR ]
-then
-    mkdir $AWS_DIR
-    echo """[default]
+config_aws(){
+
+mkdir $AWS_DIR
+echo """[default]
 aws_access_key_id = $ACCESS_KEY
 aws_secret_access_key = $SECRET_KEY""" >> $AWS_DIR/credentials
-#    echo """[default]
-#region = $DEFAULT_REGION""" >> $AWS_DIR/config
-    chown $SYS_USERNAME:$SYS_USERNAME -R $AWS_DIR/
-    chmod 600 $AWS_DIR/credentials
-#else
-#    echo "Aws already configured in your system. Kindly remove the .aws directory from your home directory"
+chown $SYS_USERNAME:$SYS_USERNAME -R $AWS_DIR/
+chmod 600 $AWS_DIR/credentials
+
+}
+
+if [ ! -d $AWS_DIR ]; then
+    config_aws
+else
+    rm -rf $AWS_DIR
+    config_aws
 fi
