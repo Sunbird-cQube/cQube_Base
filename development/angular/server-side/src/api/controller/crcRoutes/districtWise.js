0e9f805ae786ceecec6caf24667bff1f0e713e40
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2020-06-18 20:13:19 +0530

    CRC data updated

--- development/angular/server-side/src/api/controller/crcRoutes/districtWise.js
+++ development/angular/server-side/src/api/controller/crcRoutes/districtWise.js
@@ -2,27 +2,32 @@ const router = require('express').Router();
 const { logger } = require('../../lib/logger');
 const auth = require('../../middleware/check-auth');
 var const_data = require('../../lib/config');
+var parquet = require('parquetjs-lite');
 
 router.post('/districtWise', async (req, res) => {
     try {
-        logger.info('--- crc district wise api ---');
-        const_data['getParams']['Key'] = `test/crc_district_test.json`;
-        const_data['s3'].getObject(const_data['getParams'], function (err, data) {
-            if (err) {
-                logger.error(err);
-                res.status(500).json({ errMsg: "Something went wrong" });
-            } else if (!data) {
-                logger.error("No data found in s3 file");
-                res.status(403).json({ errMsg: "No such data found" });
-            } else {
-                var myData = JSON.parse(data.Body.toString());
-                logger.info('--- crc district wise api response sent ---');
-                res.status(200).send(myData);
-            }
-        });
+        logger.info('--- crc all district wise api ---');
 
+        const_data['getParams']['Key'] = `test/crc_district_test.snappy`;
+        let reader = await parquet.ParquetReader.openS3(const_data['s3'], const_data['getParams']);
+
+        let cursor = reader.getCursor();
+        let record = null;
+
+        while (record = await cursor.next()) {
+            for (let i = 0; i < record.visits.array.length; i++) {
+                record.visits.array[i]['totalSchools'] = parseInt(record.visits.array[i].totalSchools);
+                record.visits.array[i]['districtId'] = parseInt(record.visits.array[i].districtId);
+            }
+            record.schoolsVisitedCount['totalSchoolsVisited'] = parseInt(record.schoolsVisitedCount.totalSchoolsVisited);
+            record.schoolsVisitedCount['totalSchoolsNotVisited'] = parseInt(record.schoolsVisitedCount.totalSchoolsNotVisited);
+
+            logger.info('--- crc all district api response sent ---');
+            res.status(200).send({ visits: record.visits.array, schoolsVisitedCount: record.schoolsVisitedCount });
+        }
+        await reader.close();
     } catch (e) {
-        logger.error(`Error :: ${e}`)
+        logger.error(e);
         res.status(500).json({ errMessage: "Internal error. Please try again!!" });
     }
 })
