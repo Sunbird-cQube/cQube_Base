0d34c68ddf73239359fdb936ac863a808e311bf2
VLaxmikanth <laxmikanth.vattamvar@tibilsolutions.com>
2020-07-08 15:23:20 +0530

    nifi changes

--- ansible/installation_scripts/nifi_config.py
+++ ansible/installation_scripts/nifi_config.py
@@ -1,32 +1,15 @@
-#!/usr/bin/env python
-
-import glob2
-import json
-import yaml
-
-input_dir  = "nifi_parameters/"
-output_dir = "nifi_json_files/"
-
-
+import json, yaml
 with open('config.yml','r') as yaml_property:
     yaml_config=yaml.safe_load(yaml_property)
 
+with open('nifi_params.json','r') as json_payload:
+    param=json.load(json_payload)
 
-def write_file():
+for nifi_param in param['component']['parameters']:
+    for key in nifi_param:
+        if yaml_config.get(nifi_param[key]['name']):
+            nifi_param[key]['value']=yaml_config.get(nifi_param[key]['name'])
 
-    my_list_files = glob2.glob(input_dir+"*.json");
-    for file_name in my_list_files:
-        with open(file_name,'r') as json_payload:
-            param=json.load(json_payload)
-        for nifi_param in param['component']['parameters']:
-            for key in nifi_param:
-                if yaml_config.get(nifi_param[key]['name']):
-                     nifi_param[key]['value']=yaml_config.get(nifi_param[key]['name'])
-        w = file_name.split('/')[::-1][0]
-        z = w.replace('_params','');
-        file_path = output_dir+z;
-        with open(file_path,'w') as json_config:
-            json.dump(param, json_config)
-
-write_file()
+with open('nifi.json','w') as json_config:
+    json.dump(param, json_config)
 
