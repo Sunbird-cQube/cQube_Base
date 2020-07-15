1668c17446e8bf92e53e0cfd5e1a7107960acec6
VLaxmikanth <laxmikanth.vattamvar@tibilsolutions.com>
2020-07-15 20:59:33 +0530

    ansible nifi changes

--- ansible/installation_scripts/validate.sh
+++ ansible/installation_scripts/validate.sh
@@ -51,12 +51,13 @@ echo """1. Skip the Postgres installation (Will backup the data and stores in S3
           1 )
                 read -p "Enter the database name: " bk_db_name
                 read -p "Enter the Username: " bk_db_uname
-                pg_dump -h localhost -U $bk_db_uname -W -F t $bk_db_uname > `date +%Y%m%d%H%M`$bk_db_name.tar
+                pg_dump -h localhost -U $bk_db_uname -W -F t $bk_db_name > `date +%Y%m%d%H%M`$bk_db_name.tar
                 if [[ ! $? == 0 ]]; then
                     echo "There is a problem dumping the database"; tput sgr0 ; fail=1; break;
                 fi
                 echo "Backed up the database..."
                 echo "Backup file will be uploaded to S3 bucket, once the installation completes."
+		sudo sed -i "s/- createdb/#&/g" install.yml
         break
         ;;
             2 )
@@ -66,6 +67,7 @@ echo """1. Skip the Postgres installation (Will backup the data and stores in S3
                 sudo apt-get --purge remove postgresql postgresql-doc postgresql-common -y
                 sudo apt autoremove -y
                 echo "Done."
+	break	
         ;;
             3 )
                 tput setaf 1; echo "Please backup the database and rerun the installation"; tput sgr0 ; fail=1; break;
@@ -191,46 +193,12 @@ check_aws_key(){
     fi
 }
 check_s3_bucket(){
-check_length $2
-if [[ $? == 0 ]]; then
-  check_s3_bucket_naming $2
-  if [[ $? == 0 ]]; then
-    if [[ $aws_key_status == 0 ]]; then
+if [[ $aws_key_status == 0 ]]; then
         bucketstatus=`aws s3api head-bucket --bucket "${2}" 2>&1`
-        if [ $? == 0 ]
+        if [ ! $? == 0 ]
         then
-           tput setaf 3; echo "Warning: [ $1 ]:[ $2 ] Bucket owned and already exists"; tput sgr0
-            while true; do
-            read -p "Do you want to continue with same bucket name [$2] as [$1]? (yes/no): " answer
-            case $answer in
-                yes )
-                    break
-                    ;; 
-                no ) 
-                    tput setaf 3; echo "Please change the $1 value in config.yml."; tput sgr0
-                    fail=1
-                    break
-                    ;;
-                * )
-                    echo "Please enter yer or no ";;
-            esac
-            done
-            count=0
-        elif [[ $bucketstatus == *"Not Found"* ]]; then
-            echo "Bucket name $2 is available."
-        elif [[ "$bucketstatus" == *"Forbidden"* ]]; then
-            tput setaf 1; echo "Error: [ $1 : $2 ] Bucket already exists but not owned. Please change the bucket name in config.yml"; tput sgr0; fail=1
-        elif [[ "$bucketstatus" == *"Bad Request"* ]]; then
-            tput setaf 1; echo "Error: [ $1 : $2 ] Bucket name should be between 3 and 63 characters. Please change the bucket name in config.yml"; tput sgr0; fail=1
-        else
-            tput setaf 1; echo "Error: [ $1 : $2 ] $bucketstatus"; tput sgr0; fail=1
+            echo "Error: [ $1 : $2 ] Bucket not owned or not found. Please change the bucket name in config.yml"; fail=1
         fi
-    fi
-  else
-      echo "Error - $1 S3 Bucket name value is not as per naming convention. Please change the bucket name."; fail=1
-  fi
- else
-        echo "Error - Length of the value $1 is not correct. Provide the length between 3 and 63."; fail=1
 fi
 }
 
@@ -354,7 +322,8 @@ case $key in
           check_sys_user $key $value
        fi
        ;;
-   base_dir)
+   base_dir)	 
+       base_dir_temp=$value
        if [[ $value == "" ]]; then
           echo "Error - Value for $key cannot be empty. Please fill this value"; fail=1
        else
@@ -474,9 +443,9 @@ case $key in
            check_mem_variables $shared_buffers $work_mem $java_arg_2 $java_arg_3
        fi
        ;;
-    nifi_error_dir)
-       if [[ ! "$value" =~ ^(/opt/nifi/nifi_errors)$ ]]; then
-          echo "Error - Valid values for $key is /opt/nifi/nifi_errors"; fail=1
+   nifi_error_dir)
+       if [[ ! "$value" ==  "\"{{base_dir}}/cqube/nifi/nifi_errors\"" ]]; then
+          echo "Error - Valid values for $key is $base_dir_temp/cqube/nifi/nifi_errors"; fail=1
        fi
        ;;
    aws_default_region)
--- development/python/views.py
+++ development/python/views.py
@@ -1,5 +1,6 @@
-from flask import abort
+from flask import abort, Response, request, send_from_directory
 from flask_restful import reqparse
+from werkzeug.utils import secure_filename
 from flask_jwt import JWT, jwt_required, current_identity
 from config import app
 from users import User
@@ -7,32 +8,31 @@ from models import Users
 from config import bcrypt
 from env import *
 import logging
-import boto3
 from datetime import datetime
 from botocore.client import Config
 from botocore.exceptions import ClientError
+from boto3 import client
+from os.path import exists, join
+from os import path, walk
+from json import dumps, load
+import pandas as pd
+from io import BytesIO, StringIO
 
-s3_signature ={
-    'v4':'s3v4',
-    'v2':'s3'
-}
-
-AWS_ACCESS_KEY = AWS_ACCESS_KEY
-AWS_SECRET_KEY = AWS_SECRET_KEY
-AWS_DEFAULT_REGION =  AWS_DEFAULT_REGION
+def get_s3_client():
+    s3_signature = {
+        'v4': 's3v4',
+        'v2': 's3'
+    }
+    return client('s3',
+                     aws_access_key_id=AWS_ACCESS_KEY,
+                     aws_secret_access_key=AWS_SECRET_KEY,
+                     config=Config(signature_version=s3_signature['v4']),
+                     region_name=AWS_DEFAULT_REGION
+                     )
 
 def create_presigned_post(bucket_name, object_name,
                           fields=None, conditions=None, expiration=3600):
-    s3_signature ={
-    'v4':'s3v4',
-    'v2':'s3'
-}
-    s3_client = boto3.client('s3',
-                             aws_access_key_id=AWS_ACCESS_KEY,
-                             aws_secret_access_key=AWS_SECRET_KEY,
-                             config=Config(signature_version=s3_signature['v4']),
-                             region_name=AWS_DEFAULT_REGION
-                             )
+    s3_client = get_s3_client()
 
     try:
         response = s3_client.generate_presigned_post(bucket_name,
@@ -59,9 +59,7 @@ def active_user(username):
     existing_person = Users.query \
         .filter(Users.user_email == username) \
         .filter(Users.user_status == 1) \
-        .filter(Users.role_id == 5) \
-        .filter(Users.user_validity_start_date <= datetime.now().strftime('%Y-%m-%d %H:%M:%S')) \
-        .filter(Users.user_validity_end_date >= datetime.now().strftime('%Y-%m-%d %H:%M:%S')) \
+        .filter((Users.role_id == 5) | (Users.role_id == 1)) \
         .all()
     if existing_person:
         return existing_person
@@ -97,6 +95,173 @@ def aws_upload_url():
     filename = parser.add_argument("filename")
     args = parser.parse_args()
     if args["filename"]:
-        return create_presigned_post(BUCKET_NAME,str(args["filename"]))
+        return create_presigned_post(EMISSION_BUCKET_NAME,str(args["filename"]))
     else:
         abort(400, f'Bad request, validate the payload')
+
+@app.route('/download_url',methods=['POST'])
+@jwt_required()
+def aws_file_download():
+    s3_client = get_s3_client()
+    parser = reqparse.RequestParser()
+    filename = parser.add_argument("filename")
+    bucket = parser.add_argument("bucket")
+    args = parser.parse_args()
+    bucket_name = get_bucket_name(args["bucket"])
+    if args["filename"]:
+        file_name=args["filename"]
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                     Params={'Bucket':bucket_name,'Key':file_name},
+                                                     ExpiresIn=300)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    return response
+
+
+@app.route('/download_uri',methods=['POST'])
+@jwt_required()
+def aws_file_downloads():
+    s3_client = get_s3_client()
+    parser = reqparse.RequestParser()
+    filename = parser.add_argument("filename", action='append')
+    bucket = parser.add_argument("bucket")
+    args = parser.parse_args()
+    bucket_name = get_bucket_name(args["bucket"])
+    if args["filename"]:
+        file_name=args.get("filename")
+    file_resp=dict()
+    files=args["filename"]
+    for file in files:
+        print(file)
+        try:
+            response = s3_client.generate_presigned_url('get_object',
+                                                         Params={'Bucket':bucket_name,'Key':file},
+                                                         ExpiresIn=300)
+            file_resp[file]=response
+            print(file_resp,file)
+        except ClientError as e:
+            logging.error(e)
+            return None
+    return file_resp
+
+def get_total_bytes(s3,bucket_name,file_name):
+    result = s3.list_objects(Bucket=bucket_name)
+    for item in result['Contents']:
+        if item['Key'] == file_name:
+            return item['Size']
+
+def get_object(s3,bucket_name,file_name, total_bytes):
+    if total_bytes > 1000000:
+        return get_object_range(s3, bucket_name, file_name, total_bytes)
+    return s3.get_object(Bucket=bucket_name, Key=file_name)['Body'].read()
+
+def get_object_range(s3,bucket_name, file_name, total_bytes):
+    offset = 0
+    while total_bytes > 0:
+        end = offset + 999999 if total_bytes > 1000000 else ""
+        total_bytes -= 1000000
+        byte_range = 'bytes={offset}-{end}'.format(offset=offset, end=end)
+        offset = end + 1 if not isinstance(end, str) else None
+        yield s3.get_object(Bucket=bucket_name, Key=file_name, Range=byte_range)['Body'].read()
+
+def get_bucket_name(bucket):
+    if bucket==INPUT_BUCKET_NAME or bucket==OUTPUT_BUCKET_NAME or bucket==EMISSION_BUCKET_NAME:
+        return bucket
+    else:
+        abort(400, f'Bad request, validate the bucket name')
+
+def valid_path(filename):
+    full_path = join(BASE_DIR, filename)
+    if not exists(full_path):
+        return abort(404, f'Bad request, validate the filename in payload')
+
+@app.route('/list_s3_files',methods=['POST'])
+@jwt_required()
+def list_s3_files():
+    parser = reqparse.RequestParser()
+    bucket = parser.add_argument("bucket")
+    args = parser.parse_args()
+    bucket_name = get_bucket_name(args["bucket"])
+    s3_client = get_s3_client()
+    return s3_client.list_objects(Bucket=bucket_name)
+
+@app.route('/download', methods=['POST'])
+@jwt_required()
+def index():
+    s3 = get_s3_client()
+    parser = reqparse.RequestParser()
+    filename = parser.add_argument("filename")
+    bucket = parser.add_argument("bucket")
+    args = parser.parse_args()
+    bucket_name = get_bucket_name(args["bucket"])
+    if args["filename"]:
+        total_bytes = get_total_bytes(s3,bucket_name,args["filename"])
+        return Response(
+            get_object(s3, bucket_name, args["filename"], total_bytes),
+            mimetype='text/plain',
+            headers={"Content-Disposition": "attachment;filename={}".format(args["filename"].split('/')[-1])}
+        )
+    else:
+        print(args["filename"])
+        abort(400, f'Bad request, validate the filename in payload')
+
+@app.route('/list_s3_buckets',methods=['GET'])
+@jwt_required()
+def list_s3_buckets():
+    return dumps({"input":INPUT_BUCKET_NAME,"output":OUTPUT_BUCKET_NAME,"emission":EMISSION_BUCKET_NAME})
+
+@app.route('/list_log_files',methods=['GET'])
+@jwt_required()
+def files_list(file_path=BASE_DIR):
+    file_list=list()
+    for root, dirs, files in walk(file_path):
+        file_list.append({"path": path.relpath(root, file_path), "directories": dirs, "files": files})
+    return dumps(file_list)
+
+@app.route('/log-download', methods=['POST'])
+@jwt_required()
+def log_download():
+    parser = reqparse.RequestParser()
+    filename = parser.add_argument("filename")
+    args = parser.parse_args()
+    if args["filename"]:
+        valid_path(args["filename"])
+        return send_from_directory(BASE_DIR,filename=args["filename"], as_attachment=True)
+    else:
+        abort(400, f'Bad request, filename not provided in payload')
+
+def validate_weights(wdf):
+    s3_client = get_s3_client()
+    try:
+        infra_file=s3_client.get_object(Bucket=OUTPUT_BUCKET_NAME,
+                             Key="infrastructure_score/infrastructure_score.csv")
+        df = pd.read_csv(BytesIO(infra_file['Body'].read()))
+        if df["score"].sum() == 100:
+            if list(df.columns) == list(wdf.columns):
+                return True
+            else:
+                abort(400, f'Bad files, please check the file header')
+        else:
+            abort(400, f'Bad file, validate the score, score sum is not 100')
+    except Exception as err:
+        abort(400,err)
+
+@app.route('/infra_weights',methods=['POST'])
+@jwt_required()
+def upload_file():
+    uploaded_file = request.files['file']
+    content_type = request.mimetype
+    filename = secure_filename(uploaded_file.filename)
+    if filename:
+        s3_client = get_s3_client()
+        csv_buffer = uploaded_file.read()
+        wdf = pd.read_csv(BytesIO(csv_buffer))
+        validate_weights(wdf)
+        resp = s3_client.put_object(Body=csv_buffer,
+                          Bucket=EMISSION_BUCKET_NAME,
+                          Key="infrastructure_score/infrastructure_score.csv",
+                          ContentType=content_type)
+        return resp
