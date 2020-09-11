669b469dac8c2d09dd090a35a40631166a599042
VLaxmikanth <laxmikanth.vattamvar@tibilsolutions.com>
2020-09-12 02:22:54 +0530

    udise code

---
+++ development/angular/server-side/src/api/controller/udise-report/block-wise.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { logger } = require('../../lib/logger');
+const auth = require('../../middleware/check-auth');
+const s3File = require('../../lib/reads3File');
+
+router.post('/allBlockWise', auth.authController, async (req, res) => {
+    try {
+        logger.info('--- all blocks UDISE api ---');
+        let fileName = `udise/udise_block_wise.json`
+        var blockData = await s3File.readS3File(fileName);
+        var mydata = blockData.data;
+        logger.info('--- blocks UDISE api response sent---');
+        res.status(200).send({ data: mydata, footer: blockData.allBlocksFooter.totalSchools });
+
+    } catch (e) {
+        logger.error(`Error :: ${e}`);
+        res.status(500).json({ errMessage: "Internal error. Please try again!!" });
+    }
+})
+
+router.post('/blockWise/:distId', auth.authController, async (req, res) => {
+    try {
+        logger.info('--- block wise UDISE api ---');
+        let fileName = `udise/udise_block_wise.json`
+        var blockData = await s3File.readS3File(fileName);
+        let distId = req.params.distId
+
+        let filterData = blockData.data.filter(obj => {
+            return (obj.details.district_id == distId)
+        })
+        let mydata = filterData;
+        logger.info('--- block per dist UDISE api response sent---');
+        res.status(200).send({ data: mydata, footer: blockData.footer[`${distId}`].totalSchools });
+
+    } catch (e) {
+        logger.error(e);
+        res.status(500).json({ errMessage: "Internal error. Please try again!!" });
+    }
+});
+
+module.exports = router;
