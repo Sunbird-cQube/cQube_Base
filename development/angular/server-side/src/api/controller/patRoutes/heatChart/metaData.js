c0af4ac226a717fb7541183e39d7f2567a5fc33a
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2020-10-29 21:06:59 +0530

    changes in PAT heat chart done

--- development/angular/server-side/src/api/controller/patRoutes/heatChart/metaData.js
+++ development/angular/server-side/src/api/controller/patRoutes/heatChart/metaData.js
@@ -7,57 +7,12 @@ const helper = require('./helper');
 router.post('/metaData', async (req, res) => {
     try {
         logger.info('---PAT meta data api ---');
-
-        var data = {
-            "years": [
-                {
-                    year: 2019
-                },
-                {
-                    year: 2020
-                }
-            ],
-            "grades": [
-                {
-                    grade: "3"
-                },
-                {
-                    grade: "4"
-                },
-                {
-                    grade: "5"
-                }
-            ],
-            "subjects": [
-                {
-                    subject: "Maths"
-                },
-                {
-                    subject: "Science"
-                },
-                {
-                    subject: "Hindi"
-                },
-                {
-                    subject: "Gujarati"
-                }
-            ],
-            "examDate": [
-                {
-                    date: "27-09-2019"
-                },
-                {
-                    date: "28-09-2019"
-                },
-                {
-                    date: "24-08-2019"
-                }
-            ],
-            "viewBy": [
-                { key: "indicator_id", value: "Question Id" },
-                { key: "indicator", value: "Indicator" }
-            ]
-        }
+        let fileName = `pat/heatChart/metaData.json`
+        var data = await s3File.readS3File(fileName);
+        data[0].data["viewBy"] = [
+            { key: "indicator_id", value: "Question Id" },
+            { key: "indicator", value: "Indicator" }
+        ]
 
         logger.info('--- PAT meta data response sent ---');
         res.status(200).send({ data });
