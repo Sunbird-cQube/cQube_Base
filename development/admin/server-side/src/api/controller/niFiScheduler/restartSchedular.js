bcbd142f89489c9c1ad39061054c2ef8391a52ac
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2020-11-28 17:09:13 +0530

    Sem change issue fixed

--- development/admin/server-side/src/api/controller/niFiScheduler/restartSchedular.js
+++ development/admin/server-side/src/api/controller/niFiScheduler/restartSchedular.js
@@ -31,6 +31,8 @@ exports.restartNifiProcess = async function () {
                 stopTime = `${myJob.mins} ${myJob.timeToStop} * * *`;
             }
 
+            console.log(schedulerTime, ":::", stopTime);
+
             if (myJob.state == "RUNNING") {
                 logger.info('Rescheduling jobs due to nodejs restart');
 
