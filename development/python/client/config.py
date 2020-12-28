1d31a6673309596599ab48fce6b0e096f3365209
chetan <chetan.goudar@tibilsolution.com>
2020-12-28 14:09:05 +0530

    logs

--- development/python/client/config.py
+++ development/python/client/config.py
@@ -1,3 +1,3 @@
-access_token=""   #API Key
-file_path=""      #'/home/ubuntu/cqube_emission/'
-emission_url=""   #http://ip:5000/
+access_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJDZE5qcVR0N2xxd2t6RG1pWEExWGgxZEttTkl3NWJlOCIsImlhdCI6MTYwOTEyNzA3MX0.5058zbPnlxBfdAo-QPdPZ01wQ84IZfbUWPXcO-ncCAY"   #API Key
+file_path="/home/ubuntu/nifi/cqube_emission/cqube_emission_1.7_data/block_master"      #'/home/ubuntu/cqube_emission/'
+emission_url="https://cqube.tibilprojects.com/data/"   #http://ip:5000/
