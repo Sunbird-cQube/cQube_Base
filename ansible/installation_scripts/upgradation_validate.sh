2c2515fb926b164ee72a1648a1ae30639e2cc4cf
VLaxmikanth <laxmikanth.vattamvar@tibilsolutions.com>
2021-01-20 20:11:25 +0530

    added readonly database user

--- ansible/installation_scripts/upgradation_validate.sh
+++ ansible/installation_scripts/upgradation_validate.sh
@@ -192,6 +192,29 @@ check_ip()
     fi
 }
 
+check_vpn_ip()
+{
+    local ip=$2
+    ip_stat=1
+    ip_pass=0
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        ip_stat=$?
+        if [[ ! $ip_stat == 0 ]]; then
+            echo "Error - Invalid value for $key"; fail=1
+            ip_pass=0
+        fi
+    else
+        echo "Error - Invalid value for $key"; fail=1
+    fi
+}
+
 check_aws_key(){
     aws_key_status=0
     export AWS_ACCESS_KEY_ID=$1
@@ -220,6 +243,31 @@ else
    "Error - Since unable to check Postgres, credentials could not verified."; fail=1
 fi
 }
+
+# Only for release 1.9
+check_readonly_db_naming(){
+check_length $2
+if [[ $? == 0 ]]; then
+    if [[ ! $2 =~ ^[A-Za-z_]*[^_0-9\$\@\#\%\*\-\^\?]$ ]]; then
+        echo "Error - Naming convention is not correct. Please change the value of $1."; fail=1
+    fi
+else
+    echo "Error - Length of the value $1 is not correct. Provide the length between 3 and 63."; fail=1
+fi
+}
+
+check_readonly_db_password(){
+    len="${#2}"
+    if test $len -ge 8 ; then
+        echo "$2" | grep "[A-Z]" | grep "[a-z]" | grep "[0-9]" | grep "[@#$%^&*]" > /dev/null 2>&1
+        if [[ ! $? -eq 0 ]]; then
+            echo "Error - $1 should contain atleast one uppercase, one lowercase, one special character and one number. And should be minimum of 8 characters."; fail=1
+        fi
+    else
+        echo "Error - $1 should contain atleast one uppercase, one lowercase, one special character and one number. And should be minimum of 8 characters."; fail=1
+    fi
+}
+# Only for release 1.9
 
 check_api_endpoint(){
 temp_ep=`grep '^KEYCLOAK_HOST =' $base_dir/cqube/dashboard/server_side/.env | awk '{print $3}' | sed s/\"//g`
@@ -286,9 +334,10 @@ echo -e "\e[0;33m${bold}Validating the config file...${normal}"
 
 
 # An array of mandatory values
-declare -a arr=("diksha_columns" "state_code" "static_datasource" "system_user_name" "base_dir" "db_user" "db_name" "db_password" "s3_access_key" \
-		"s3_secret_key" "s3_input_bucket" "s3_output_bucket" "s3_emission_bucket" \
-		"aws_default_region" "local_ipv4_address" "api_endpoint" "keycloak_adm_passwd" "keycloak_adm_user" "keycloak_config_otp" "session_timeout")
+declare -a arr=("diksha_columns" "state_code" "static_datasource" "system_user_name" "base_dir" "db_user" "db_name" "db_password" "read_only_db_user" \
+                "read_only_db_password" "s3_access_key" "s3_secret_key" "s3_input_bucket" "s3_output_bucket" "s3_emission_bucket" \
+		"aws_default_region" "local_ipv4_address" "vpn_local_ipv4_address" "api_endpoint" "keycloak_adm_passwd" "keycloak_adm_user" \
+		"keycloak_config_otp" "session_timeout")
 
 # Create and empty array which will store the key and value pair from config file
 declare -A vals
@@ -404,6 +453,13 @@ case $key in
           check_ip $key $value
        fi
        ;;
+   vpn_local_ipv4_address)
+       if [[ $value == "" ]]; then
+          echo "Error - in $key. Unable to get the value. Please check."; fail=1
+       else
+          check_vpn_ip $key $value
+       fi
+       ;;
    db_user)
        if [[ $value == "" ]]; then
           echo "Error - in $key. Unable to get the value. Please check."; fail=1
@@ -417,6 +473,13 @@ case $key in
           echo "Error - in $key. Unable to get the value. Please check."; fail=1
        else
           check_db_naming $key $value CQUBE_DB_NAME
+       fi
+       ;;
+   read_only_db_user)
+       if [[ $value == "" ]]; then
+          echo "Error - in $key. Unable to get the value. Please check."; fail=1
+       else
+          check_readonly_db_naming $key $value CQUBE_DB_NAME
        fi
        ;;
    keycloak_adm_user)
@@ -443,6 +506,13 @@ case $key in
           echo "Error - in $key. Unable to get the value. Please check."; fail=1
        else
           check_db_password $db_name $db_user $db_password
+       fi
+       ;;
+   read_only_db_password)
+       if [[ $value == "" ]]; then
+          echo "Error - in $key. Unable to get the value. Please check."; fail=1
+       else
+          check_readonly_db_password $db_name $db_user $db_password
        fi
        ;;
    api_endpoint)
