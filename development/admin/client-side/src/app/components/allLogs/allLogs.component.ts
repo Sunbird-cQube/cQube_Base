4c1c6733d4a0ebc609a6e1a2fd28f031e8b21cb2
panduankani <panduankani@gmail.com>
2021-02-02 12:40:12 +0530

    Changes in log file module of admin

--- development/admin/client-side/src/app/components/allLogs/allLogs.component.ts
+++ development/admin/client-side/src/app/components/allLogs/allLogs.component.ts
@@ -28,6 +28,7 @@ export class AllLogsComponent implements OnInit {
   fileView = false;
   previousFiles: any[] = [];
   loadingPreviousFiles = false;
+  selectedFile: any;
 
   constructor(private router: Router, private service: LogsService) {
     this.showErr = false;
@@ -58,7 +59,6 @@ export class AllLogsComponent implements OnInit {
     this.fileView = false;
     this.previousFiles = [];
     this.service.getLogMenu().subscribe((res: any) => {
-      console.log(res);
       res.forEach(element => {
         if (element.name == type) {
           element.children.forEach(item => {
@@ -77,9 +77,11 @@ export class AllLogsComponent implements OnInit {
     this.fileHidden = false;
     this.fileView = false;
     this.previousFiles = [];
-    this.service.showLogs(type).subscribe(res => {
-      this.title = res['title'];
-      this.logPath = res['path'];
+    this.service.showLogs(this.logTypeName, type).subscribe((res: any) => {
+      this.previousFiles = res;
+      this.selectedFile = res[0];
+      this.title = res[0]['title'];
+      this.logPath = res[0]['path'];
       this.showLog(this.logPath);
       setTimeout(() => {
         document.getElementById('spinner').style.display = 'none';
@@ -103,9 +105,8 @@ export class AllLogsComponent implements OnInit {
       setTimeout(() => {
         document.getElementById('spinner').style.display = 'none';
       }, 300);
-    })
+    });
   }
-
 
   downloadLogs() {
     document.getElementById('spinner').style.display = 'block';
@@ -129,14 +130,15 @@ export class AllLogsComponent implements OnInit {
     });
   }
 
-  downloadLogFile(fileName: string): void {
-    this.service.downloadLogFile(fileName).subscribe(res => {
+  downloadLogFile(): void {
+    document.getElementById('spinner').style.display = 'block';
+    this.service.downloadLogFile(this.selectedFile).subscribe(res => {
       const fileName = getFileNameFromResponseContentDisposition(res);
       const mimeType = getMimeTypeFromResponseContentDisposition(res);
       saveFile(res.body, fileName, mimeType);
+      document.getElementById('spinner').style.display = 'none';
     });
   }
-
 
   //for download txt file
 
