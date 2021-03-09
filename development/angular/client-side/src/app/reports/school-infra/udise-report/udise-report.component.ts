4c40e763e2cafb7f227a238b69e878fd58d08dc5
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2021-03-10 01:47:23 +0530

    PAT, SAT, TAR and SAR map reports UI changes done

--- development/angular/client-side/src/app/reports/school-infra/udise-report/udise-report.component.ts
+++ development/angular/client-side/src/app/reports/school-infra/udise-report/udise-report.component.ts
@@ -101,20 +101,23 @@ export class UdiseReportComponent implements OnInit {
   colorGenData: any = [];
 
   width = window.innerWidth;
+  heigth = window.innerHeight;
   onResize(event) {
     this.width = window.innerWidth;
-    this.commonService.zoomLevel = this.width > 2560 ? this.commonService.mapCenterLatlng.zoomLevel + 1 : this.width < 2560 && this.width > 1920 ? this.commonService.mapCenterLatlng.zoomLevel + 1 : this.commonService.mapCenterLatlng.zoomLevel;
+    this.heigth = window.innerHeight;
+    this.commonService.zoomLevel = this.width > 3820 ? this.commonService.mapCenterLatlng.zoomLevel + 2 : this.width < 3820 && this.width >= 2500 ? this.commonService.mapCenterLatlng.zoomLevel + 1 : this.width < 2500 && this.width > 1920 ? this.commonService.mapCenterLatlng.zoomLevel + 1 : this.commonService.mapCenterLatlng.zoomLevel;
+    this.changeDetection.detectChanges();
     this.levelWiseFilter();
   }
   setZoomLevel(lat, lng, globalMap, zoomLevel) {
+    this.changeDetection.detectChanges();
     globalMap.setView(new L.LatLng(lat, lng), zoomLevel);
     globalMap.options.minZoom = this.commonService.zoomLevel;
   }
-  getMarkerRadius(rad1, rad2, rad3) {
-    let radius = this.width > 2560 ? rad1 : this.width < 2560 && this.width > 1920 ? rad2 : rad3;
+  getMarkerRadius(rad1, rad2, rad3, rad4) {
+    let radius = this.width > 3820 ? rad1 : this.width > 2500 && this.width < 3820 ? rad2 : this.width < 2500 && this.width > 1920 ? rad3 : rad4;
     return radius;
   }
-
 
   ngOnInit() {
     this.state = this.commonService.state;
@@ -122,7 +125,6 @@ export class UdiseReportComponent implements OnInit {
     this.lng = this.commonService.mapCenterLatlng.lng;
     this.changeDetection.detectChanges();
     this.commonService.initMap('udisemap', [[this.lat, this.lng]]);
-    // globalMap.setMaxBounds([[this.lat - 4.5, this.lng - 6], [this.lat + 3.5, this.lng + 6]]);
     document.getElementById('homeBtn').style.display = 'block';
     document.getElementById('backBtn').style.display = 'none';
     let params = JSON.parse(sessionStorage.getItem('report-level-info'));
@@ -253,7 +255,7 @@ export class UdiseReportComponent implements OnInit {
         this.districtMarkers = this.myDistData['data'];
         // options to set for markers in the map
         let options = {
-          radius: this.getMarkerRadius(10, 8, 5),
+          radius: this.getMarkerRadius(14, 10, 8, 5),
           fillOpacity: 1,
           strokeWeight: 0.05,
           mapZoom: this.commonService.zoomLevel,
@@ -281,7 +283,7 @@ export class UdiseReportComponent implements OnInit {
 
           // options to set for markers in the map
           let options = {
-            radius: this.getMarkerRadius(10, 8, 5),
+            radius: this.getMarkerRadius(14, 10, 8, 5),
             fillOpacity: 1,
             strokeWeight: 0.01,
             mapZoom: this.commonService.zoomLevel,
@@ -366,7 +368,7 @@ export class UdiseReportComponent implements OnInit {
                 this.setColor = this.commonService.relativeColorGredient(this.blockMarkers[i], this.indiceData, colors);
               }
               var markerIcon = L.circleMarker([this.blockMarkers[i].details.latitude, this.blockMarkers[i].details.longitude], {
-                radius: this.getMarkerRadius(6.5, 5, 4),
+                radius: this.getMarkerRadius(8, 6.5, 5, 4),
                 color: "gray",
                 fillColor: this.setColor,
                 fillOpacity: 1,
@@ -455,7 +457,7 @@ export class UdiseReportComponent implements OnInit {
                 this.setColor = this.commonService.relativeColorGredient(this.clusterMarkers[i], this.indiceData, colors);
               }
               var markerIcon = L.circleMarker([this.clusterMarkers[i].details.latitude, this.clusterMarkers[i].details.longitude], {
-                radius: this.getMarkerRadius(2.5, 2.2, 2),
+                radius: this.getMarkerRadius(4.5, 2.5, 2.2, 2),
                 color: "gray",
                 fillColor: this.setColor,
                 fillOpacity: 1,
@@ -545,7 +547,7 @@ export class UdiseReportComponent implements OnInit {
               }
               var markerIcon = L.circleMarker([this.schoolMarkers[i].details.latitude, this.schoolMarkers[i].details.longitude], {
                 // renderer: myRenderer,
-                radius: this.getMarkerRadius(1.5, 1.2, 1),
+                radius: this.getMarkerRadius(2, 1.5, 1.2, 1),
                 color: "gray",
                 fillColor: this.setColor,
                 fillOpacity: 1,
@@ -625,7 +627,7 @@ export class UdiseReportComponent implements OnInit {
 
       // options to set for markers in the map
       let options = {
-        radius: this.getMarkerRadius(10, 8, 4.5),
+        radius: this.getMarkerRadius(14, 10, 8, 4.5),
         fillOpacity: 1,
         strokeWeight: 0.01,
         mapZoom: this.commonService.zoomLevel + 1,
@@ -698,7 +700,7 @@ export class UdiseReportComponent implements OnInit {
 
       // options to set for markers in the map
       let options = {
-        radius: this.getMarkerRadius(10, 8, 4.5),
+        radius: this.getMarkerRadius(14, 10, 8, 4.5),
         fillOpacity: 1,
         strokeWeight: 0.01,
         mapZoom: this.commonService.zoomLevel + 3,
@@ -784,7 +786,7 @@ export class UdiseReportComponent implements OnInit {
 
         // options to set for markers in the map
         let options = {
-          radius: this.getMarkerRadius(10, 8, 4.5),
+          radius: this.getMarkerRadius(14, 10, 8, 4.5),
           fillOpacity: 1,
           strokeWeight: 0.01,
           mapZoom: this.commonService.zoomLevel + 5,
@@ -1025,6 +1027,7 @@ export class UdiseReportComponent implements OnInit {
     if (this.level == 'school') {
       this.onClusterSelect(this.clusterId);
     }
+    this.setZoomLevel(this.lat, this.lng, globalMap, this.commonService.zoomLevel);
   }
 
   //map tooltip automation
