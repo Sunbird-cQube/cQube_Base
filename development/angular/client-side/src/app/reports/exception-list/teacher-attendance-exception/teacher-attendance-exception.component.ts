ed2674bd5406f2c79b5d764995d42031af4883c4
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2021-03-11 21:29:39 +0530

    All bar charts and Composite chart report UI changes done

--- development/angular/client-side/src/app/reports/exception-list/teacher-attendance-exception/teacher-attendance-exception.component.ts
+++ development/angular/client-side/src/app/reports/exception-list/teacher-attendance-exception/teacher-attendance-exception.component.ts
@@ -92,6 +92,7 @@ export class TeacherAttendanceExceptionComponent implements OnInit {
     this.commonService.zoomLevel = this.commonService.mapCenterLatlng.zoomLevel;
     this.commonService.initMap('tarExpMap', [[this.lat, this.lng]]);
     globalMap.setMaxBounds([[this.lat - 4.5, this.lng - 6], [this.lat + 3.5, this.lng + 6]]);
+    globalMap.options.minZoom = this.commonService.zoomLevel;
     document.getElementById('homeBtn').style.display = 'block';
     document.getElementById('backBtn').style.display = 'none';
     this.skul = true;
