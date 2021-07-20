517925597167e4c1aaccaaf61500696c17f70ef1
jp-tibil <jeyaprakash@tibilsolutions.com>
2021-07-20 11:27:02 +0530

    base code for installtion and upgradation of 3.0

--- validate.sh
+++ validate.sh
@@ -177,14 +177,9 @@ check_db_password(){
 }
 
 check_storage_type(){
-if  [[ $2 == "s3" || $2 == "local" ]]; then
-    if [[ $2 == "s3" ]]; then
-       echo "validating aws_s3_validate.sh"
-       . "aws_s3_validate.sh"
-    fi
-else 
+if ! [[ $2 == "s3" || $2 == "local" ]]; then
     echo "Error - Please enter either s3 or local for $1"; fail=1
-    fi
+fi
 }
 
 check_api_endpoint(){
