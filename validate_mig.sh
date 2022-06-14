f902f86f3ecf2791dbfbf55a467b317c91a9de73
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2022-06-14 17:56:33 +0530

    validate_mig.sh

--- validate_mig.sh
+++ validate_mig.sh
@@ -24,7 +24,26 @@ if ! [[ $2 == "true" || $2 == "false" ]]; then
 fi
 }
 
+check_postgres(){
+echo "Checking for Postgres ..."
+temp=$(psql -V > /dev/null 2>&1; echo $?)
 
+if [ $temp == 0 ]; then
+    version=`psql -V | head -n1 | cut -d" " -f3`
+    if [[ $(echo "$version >= 10.12" | bc) == 1 ]]
+    then
+        echo "WARNING: Postgres found."
+        echo "Removing Postgres..."
+        sudo systemctl stop kong.service > /dev/null 2>&1
+        sleep 5
+        sudo systemctl stop keycloak.service > /dev/null 2>&1
+        sleep 5
+        sudo systemctl stop postgresql
+        sudo apt-get --purge remove postgresql* -y
+        echo "Done"
+     fi
+fi
+}
 check_mem(){
 mem_total_kb=`grep MemTotal /proc/meminfo | awk '{print $2}'`
 mem_total=$(($mem_total_kb/1024))
