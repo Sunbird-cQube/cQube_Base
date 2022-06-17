83d7fdc434b86bda88c454b193b32ec264f3a1e5
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2022-06-17 12:05:53 +0530

    aws-cli code updated

--- data_mig.sh
+++ data_mig.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 VAL_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$VAL_DIR" ]]; then
-        VAL_DIR="$PWD";
-fi
+if [[ ! -d "$VAL_DIR" ]]; then VAL_DIR="$PWD"; fi
 
 aws s3api help > /dev/null 2>&1
 if [ ! $? == 0 ]; then
 
 echo "Installing aws cli ..."
-wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip  > /dev/null 2>&1
-echo "A" | unzip awscli-bundle.zip  > /dev/null 2>&1
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"  > /dev/null 2>&1
+sudo apt install unzip
+echo "A" | unzip awscliv2.zip  > /dev/null 2>&1
 echo "Configuring aws cli ..."
-python3 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws  > /dev/null 2>&1
-python3 ./awscli-bundle/install -b ~/bin/aws  > /dev/null 2>&1
-python3 ./awscli-bundle/install -h  > /dev/null 2>&1
+./aws/install  > /dev/null 2>&1
+./aws/install -i /usr/local/aws-cli -b /usr/local/bin > /dev/null 2>&1
 fi
 
 storage_type=$(awk ''/^storage_type:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
@@ -29,10 +27,14 @@ fi
 
 if [[ $remote_storage_type = "s3" ]]; then
 output_bucket=$(awk ''/^s3_output_bucket:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
+aws_access_key=$(awk ''/^s3_access_key:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
+aws_secret_key=$(awk ''/^s3_secret_key:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
 fi
 
 bucket=$(cat $base_dir/cqube/.cqube_config | grep CQUBE_S3_OUTPUT )
 source_bucket=$(cut -d "=" -f2 <<< "$bucket")
+
+
 
 export AWS_ACCESS_KEY_ID=$aws_access_key
 export AWS_SECRET_ACCESS_KEY=$aws_secret_key
--- local_to_s3.sh
+++ local_to_s3.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 VAL_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$VAL_DIR" ]]; then
-        VAL_DIR="$PWD";
-fi
+if [[ ! -d "$VAL_DIR" ]]; then VAL_DIR="$PWD"; fi
 
 aws s3api help > /dev/null 2>&1
 if [ ! $? == 0 ]; then
 
 echo "Installing aws cli ..."
-wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip  > /dev/null 2>&1
-echo "A" | unzip awscli-bundle.zip  > /dev/null 2>&1
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"  > /dev/null 2>&1
+sudo apt install unzip
+echo "A" | unzip awscliv2.zip  > /dev/null 2>&1
 echo "Configuring aws cli ..."
-python3 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws  > /dev/null 2>&1
-python3 ./awscli-bundle/install -b ~/bin/aws  > /dev/null 2>&1
-python3 ./awscli-bundle/install -h  > /dev/null 2>&1
+./aws/install  > /dev/null 2>&1
+./aws/install -i /usr/local/aws-cli -b /usr/local/bin > /dev/null 2>&1
 fi
 
 storage_type=$(awk ''/^storage_type:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
@@ -22,6 +20,10 @@ storage_type=$(awk ''/^storage_type:' /{ if ($2 !~ /#.*/) {print $2}}' config.ym
 remote_storage_type=$(awk ''/^remote_storage_type:' /{ if ($2 !~ /#.*/) {print $2}}' migrate_config.yml)
 
 output_bucket=$(awk ''/^s3_output_bucket:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
+
+aws_access_key=$(awk ''/^s3_access_key:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
+aws_secret_key=$(awk ''/^s3_secret_key:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
+
 
 dir=$(cat $base_dir/cqube/.cqube_config | grep CQUBE_OUTPUT_DIRECTORY )
 source_output_directory=$(cut -d "=" -f2 <<< "$dir")
