3be44d8451af449ef1d428c3eed4d5bddc724648
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2022-06-22 18:25:18 +0530

    migration code updated for input and emission

--- data_mig.sh
+++ data_mig.sh
@@ -24,28 +24,40 @@ base_dir=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
 
 if [[ $storage_type = "local" ]]; then
 output_directory=$(awk ''/^output_directory:' /{ if ($2 !~ /#.*/) {print $2}}' local_storage_config.yml)
+input_directory=$(awk ''/^input_directory:' /{ if ($2 !~ /#.*/) {print $2}}' local_storage_config.yml)
+emission_directory=$(awk ''/^emission_directory:' /{ if ($2 !~ /#.*/) {print $2}}' local_storage_config.yml)
 fi
 
 if [[ $storage_type = "s3" ]]; then
 output_bucket=$(awk ''/^s3_output_bucket:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
+input_bucket=$(awk ''/^s3_input_bucket:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
+emission_bucket=$(awk ''/^s3_emission_bucket:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
+
 aws_access_key=$(awk ''/^s3_access_key:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
 aws_secret_key=$(awk ''/^s3_secret_key:' /{ if ($2 !~ /#.*/) {print $2}}' aws_s3_config.yml)
 fi
 
 bucket=$(cat /home/$system_user_name/.cqube_config | grep CQUBE_S3_OUTPUT )
-source_bucket=$(cut -d "=" -f2 <<< "$bucket")
+out_bucket=$(cut -d "=" -f2 <<< "$bucket")
 
+bucket=$(cat /home/$system_user_name/.cqube_config | grep CQUBE_S3_INPUT )
+in_bucket=$(cut -d "=" -f2 <<< "$bucket")
 
-echo "source_bucket"
+bucket=$(cat /home/$system_user_name/.cqube_config | grep CQUBE_S3_EMISSION )
+emi_bucket=$(cut -d "=" -f2 <<< "$bucket")
+
 export AWS_ACCESS_KEY_ID=$aws_access_key
 export AWS_SECRET_ACCESS_KEY=$aws_secret_key
 
 if [[ $src_type = "s3" ]] && [[ $storage_type = "s3" ]]; then
-irbucket=$(aws s3 sync s3://$source_bucket s3://$output_bucket)
+irbucket=$(aws s3 sync s3://$out_bucket s3://$output_bucket)
+irbucket=$(aws s3 sync s3://$in_bucket s3://$input_bucket)
+irbucket=$(aws s3 sync s3://$emi_bucket s3://$emission_bucket)
 fi
 
 if [[ $src_type = "s3" ]] && [[ $storage_type = "local" ]]; then
-
-irbucket=$(aws s3 sync s3://$source_bucket $output_directory)
+irbucket=$(aws s3 sync s3://$out_bucket $output_directory)
+irbucket=$(aws s3 sync s3://$in_bucket $input_directory)
+irbucket=$(aws s3 sync s3://$emi_bucket $emission_directory)
 fi
 
