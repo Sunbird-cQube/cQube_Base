6034d8c1437017ad890e7fa411b399a7b8bff282
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2022-07-06 14:58:06 +0530

    on-premises migration code updated

--- validation_scripts/get_bucket_config_values.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash/
-
-base_dir=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' ~/cQube_Base/migrate_config.yml)
-s3_output_bucket=$(awk ''/^s3_output_bucket:' /{ if ($2 !~ /#.*/) {print $2}}' ~/cQube_Base/migrate_config.yml)
-bucket=$(cat $base_dir/cqube/.cqube_config | grep CQUBE_S3_OUTPUT )
-output_bucket=$(cut -d "=" -f2 <<< "$bucket")
-echo $output_bucket
-
--- validation_scripts/get_inputdir_config_values.sh
+++ validation_scripts/get_inputdir_config_values.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-base_dir=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' ~/cQube_Base/migrate_config.yml)
+base_dir=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' ~/cQube_Base/config.yml)
 
 dir=$(cat $base_dir/cqube/.cqube_config | grep CQUBE_INPUT_DIRECTORY )
 input_path=$(cut -d "=" -f2 <<< "$dir")
