bf8480e44b78e13829cbfa457d946b4d00f41a90
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2022-08-16 15:08:08 +0530

    azure migration

--- azure_to_aws.sh
+++ azure_to_aws.sh
@@ -44,29 +44,35 @@ azure_account_name=$(awk ''/^azure_account_name:' /{ if ($2 !~ /#.*/) {print $2}
 azure_account_key=$(awk ''/^azure_account_key:' /{ if ($2 !~ /#.*/) {print $2}}' azure_container_config.yml)
 storage_account_sas_token=$(awk ''/^storage_account_sas_token:' /{ if ($2 !~ /#.*/) {print $2}}' azure_container_config.yml)
 
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -xvf downloadazcopy-v10-linux
+sudo rm /usr/bin/azcopy
+sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
+
+
 str_typ=$(cat $base_dir/cqube/.cqube_config | grep CQUBE_STORAGE_TYPE )
 src_type=$(cut -d "=" -f2 <<< "$str_typ")
 
 
 if [[ $src_type = "azure" ]] && [[ $storage_type = "s3" ]]; then
-azcopy copy https://$azure_account_name.blob.core.windows.net/$azure_input_container/*$storage_account_sas_token /home/$system_user_name/azure_input --recursive
+sudo azcopy copy https://$azure_account_name.blob.core.windows.net/$azure_input_container/*$storage_account_sas_token /tmp/cqube_migration/azure_input --recursive
 fi
 
 if [[ $src_type = "azure" ]] && [[ $storage_type = "s3" ]]; then
-azcopy copy https://$azure_account_name.blob.core.windows.net/$azure_output_container/*$storage_account_sas_token /home/$system_user_name/azure_output --recursive
+sudo azcopy copy https://$azure_account_name.blob.core.windows.net/$azure_output_container/*$storage_account_sas_token /tmp/cqube_migration/azure_output --recursive
 fi
 
 if [[ $src_type = "azure" ]] && [[ $storage_type = "s3" ]]; then
-azcopy copy https://$azure_account_name.blob.core.windows.net/$azure_emissionput_container/*$storage_account_sas_token /home/$system_user_name/azure_emission --recursive
+sudo azcopy copy https://$azure_account_name.blob.core.windows.net/$azure_emission_container/*$storage_account_sas_token /tmp/cqube_migration/azure_emission --recursive
 fi
 
 
 if [[ $src_type = "azure" ]] && [[ $storage_type = "s3" ]]; then
-irbucket=$(aws s3 sync /home/$system_user_name/azure_output s3://$output_bucket)
+irbucket=$(aws s3 sync /tmp/cqube_migration/azure_output s3://$output_bucket)
 fi
 if [[ $src_type = "azure" ]] && [[ $storage_type = "s3" ]]; then
-irbucket=$(aws s3 sync /home/$system_user_name/azure_input s3://$input_bucket)
+irbucket=$(aws s3 sync /tmp/cqube_migration/azure_input s3://$input_bucket)
 fi
 if [[ $src_type = "azure" ]] && [[ $storage_type = "s3" ]]; then
-irbucket=$(aws s3 sync /home/$system_user_name/azure_emission s3://$emission_bucket)
+irbucket=$(aws s3 sync /tmp/cqube_migration/azure_emission s3://$emission_bucket)
 fi
--- azure_to_azure.sh
+++ azure_to_azure.sh
@@ -17,7 +17,6 @@ azure_account_key==$(awk ''/^azure_account_key:' /{ if ($2 !~ /#.*/) {print $2}}
 azr=$(cat $base_dir/cqube/.cqube_config | grep CQUBE_AZURE_OUTPUT_STORAGE )
 source_output_container=$(cut -d "=" -f2 <<< "$azr")
 
-echo "$source_output_container"
 azr=$(cat $base_dir/cqube/.cqube_config | grep CQUBE_AZURE_INPUT_STORAGE )
 source_input_container=$(cut -d "=" -f2 <<< "$azr")
 
@@ -31,13 +30,13 @@ src_type=$(cut -d "=" -f2 <<< "$str_typ")
 az login > /dev/null 2>&1
 
 if [[ $src_type = "azure" ]] && [[ $storage_type = "azure" ]]; then
-sudo az storage blob copy start-batch --account-key $azure_account_key --account-name $azure_account_name --destination-container $azure_output_container --source-account-key $azure_account_key --source-account-name $azure_account_name --source-container $source_output_container
+sudo az storage blob copy start-batch --account-key $azure_account_key --account-name $azure_account_name --destination-container $azure_output_container --source-account-key $azure_account_key --source-account-name $azure_account_name --source-container $source_output_container > /dev/null 2>&1
 fi
 
 if [[ $src_type = "azure" ]] && [[ $storage_type = "azure" ]]; then
-sudo az storage blob copy start-batch --account-key $azure_account_key --account-name $azure_account_name --destination-container $azure_input_container --source-account-key $azure_account_key --source-account-name $azure_account_name --source-container $source_input_container
+sudo az storage blob copy start-batch --account-key $azure_account_key --account-name $azure_account_name --destination-container $azure_input_container --source-account-key $azure_account_key --source-account-name $azure_account_name --source-container $source_input_container > /dev/null 2>&1
 fi
 
 if [[ $src_type = "azure" ]] && [[ $storage_type = "azure" ]]; then
-sudo az storage blob copy start-batch --account-key $azure_account_key --account-name $azure_account_name --destination-container $azure_emission_container --source-account-key $azure_account_key --source-account-name $azure_account_name --source-container $source_emission_container
+sudo az storage blob copy start-batch --account-key $azure_account_key --account-name $azure_account_name --destination-container $azure_emission_container --source-account-key $azure_account_key --source-account-name $azure_account_name --source-container $source_emission_container > /dev/null 2>&1
 fi
